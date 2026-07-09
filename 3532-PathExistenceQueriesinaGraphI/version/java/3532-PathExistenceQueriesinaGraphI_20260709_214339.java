358e8f36fde81c3980c9add9623be74b70cc34d9
akalya-afk <akalyaselvam2005@gmail.com>
2026-07-09 21:43:39 +0530

    Create 3532-PathExistenceQueriesinaGraphI

---
+++ 3532-PathExistenceQueriesinaGraphI/version/java/3532-PathExistenceQueriesinaGraphI_20260709_214339.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 21:43:39
+1class Solution {
+2    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] q) {
+3         boolean ans[] = new boolean[q.length]; 
+4        int root[] = new int[nums.length]; 
+5        root[0] = 0; 
+6        for(int i = 1; i < nums.length; i++) {
+7            root[i] = ((nums[i] - nums[i - 1]) <= maxDiff) ? root[i - 1] : i; 
+8        }
+9
+10        for(int i = 0; i < q.length; i++) {
+11            ans[i] = root[q[i][0]] == root[q[i][1]]; 
+12        }
+13        return ans; 
+14    }
+15}
