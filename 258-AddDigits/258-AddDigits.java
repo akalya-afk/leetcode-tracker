b9a765949decc477f2f2c847504f1e3c171422ed
akalya-afk <akalyaselvam2005@gmail.com>
2026-07-09 09:10:31 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:10:29
+class Solution {
+    public int addDigits(int num) {
+       while(num>=10){
+        int sum=0;
+         while(num>0){
+            int rem=num%10;
+            sum+=rem;
+            num=num/10;
+         }
+         num=sum;
+       }
+       return num;
+
+    }
+}
