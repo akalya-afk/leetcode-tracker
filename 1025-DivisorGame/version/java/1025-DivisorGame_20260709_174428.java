c595342e0a90701caa3d417965476d7101f69373
akalya-afk <akalyaselvam2005@gmail.com>
2026-07-09 17:44:29 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/version/java/1025-DivisorGame_20260709_174428.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 17:44:28
+1class Solution {
+2    public boolean divisorGame(int n) {
+3        if(n%2==0){
+4            return true;
+5        }
+6        return false;
+7    }
+8}
