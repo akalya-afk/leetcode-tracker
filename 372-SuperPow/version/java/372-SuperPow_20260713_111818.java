65194ac32b9e4be0186ae6c71a2e36b368ca74ea
akalya-afk <akalyaselvam2005@gmail.com>
2026-07-13 11:18:18 +0530

    Create 372-SuperPow

---
+++ 372-SuperPow/version/java/372-SuperPow_20260713_111818.java
@@ -0,0 +1,10 @@
+// Last updated: 13/07/2026, 11:18:18
+1class Solution {
+2    public int distinctIntegers(int n) {
+3       
+4        if(n==1){
+5           return n;
+6        }
+7       return n-1;
+8    }
+9}
