b1991d454b241ea291811094726113d3a88e24d8
akalya-afk <akalyaselvam2005@gmail.com>
2026-09-02 09:42:08 +0530

    Create 405-ConvertANumberToHexadecimal

---
+++ 405-ConvertANumberToHexadecimal/405-ConvertANumberToHexadecimal.java
@@ -0,0 +1,25 @@
+// Last updated: 02/09/2026, 09:42:07
+class Solution {
+
+    public String toHex(int num) {
+
+        if (num == 0)
+            return "0";
+
+        char[] hex = {
+            '0','1','2','3','4','5','6','7',
+            '8','9','a','b','c','d','e','f'
+        };
+
+        StringBuilder ans = new StringBuilder();
+
+        while (num != 0) {
+
+            ans.append(hex[num & 15]);
+
+            num >>>= 4;
+        }
+
+        return ans.reverse().toString();
+    }
+}
