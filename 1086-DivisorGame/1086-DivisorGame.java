1269ef8ad0ac6546a304bd1c4b8181be7b1fc629
akalya-afk <akalyaselvam2005@gmail.com>
2026-09-02 09:41:09 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,9 @@
+// Last updated: 02/09/2026, 09:41:08
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }
+        return false;
+    }
+}
