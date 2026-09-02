02d9da59e259e8a4af08b7ed9e4446b1e6fe6e1a
akalya-afk <akalyaselvam2005@gmail.com>
2026-09-02 09:39:50 +0530

    Create 2608-CountTheDigitsThatDivideANumber

---
+++ 2608-CountTheDigitsThatDivideANumber/2608-CountTheDigitsThatDivideANumber.java
@@ -0,0 +1,15 @@
+// Last updated: 02/09/2026, 09:39:48
+class Solution {
+    public int countDigits(int num) {
+        int temp=num;
+        int count=0;
+        while(num>0){
+            int r=num%10;
+            if(temp%r==0){
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+    }
+}
